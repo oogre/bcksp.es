37d8091627e52cc5c9475437870a502ffdac4581
oogre <vincent@ogre.be>
2014-08-22 18:41:59 +0200

    user/confirmation

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -14,11 +14,5 @@ module.exports.bootstrap = function(cb) {
   // It's very important to trigger this callback method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
 
-  User.update({}, {
-  	online : false
-  }, function userUpdated(err, users){
-  	if(err) console.log(err);
-  	cb();	
-  });
-  
+	cb();
 };
