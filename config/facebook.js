34b9e464868d2d1f6d81b5cf0188eee966e822af
oogre <vincent@ogre.be>
2014-09-05 22:11:13 +0200

    update

--- config/facebook.js
+++ config/facebook.js
@@ -2,6 +2,6 @@ module.exports = {
   facebook : {
     appId : process.env.FACEBOOK_APP_ID,
     appSecret : process.env.FACEBOOK_APP_SECRET,
-    callbackURL: "http://bcksp.es/auth/facebook/callback",
+    callbackURL: "http://www.bcksp.es/auth/facebook/callback",
   }
 }
