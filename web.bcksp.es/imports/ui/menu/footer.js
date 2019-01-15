b47553df4ead325af9a202d9018e9317fb3de488
acr <hello@crucifixarnaud.com>
2019-01-15 20:56:39 +0100

    Simple footer design (no responsive)

--- web.bcksp.es/imports/ui/menu/footer.js
+++ web.bcksp.es/imports/ui/menu/footer.js
@@ -25,42 +25,43 @@ class MenuFooter extends Component {
 	hasToDisplayDownloadBtn(){
 		return !this.props.extensionInstalled;
 	}
-	
+
 	hasToDisplayProfileBtn(){
 		return this.props.isConnected && this.props.extensionInstalled;
 	}
-	
+
 	hasToDisplayLogoutBtn(){
 		return this.props.isConnected && this.props.extensionInstalled;
 	}
-	
+
 	render() {
 			return (
 			<footer className="main-footer">
 				<div className="container">
-					<nav className="main-footer__nav">
+					<img className="main-footer__logo" src="/images/logo-glyph.svg" width="140" height="60" alt="Bcksp.es"/>
+					<nav className="main-footer__nav" role="navigation">
 						<div className="column">
 							<div className="column__content">
 								<ul className="menu menu--footer">
 									<li className="menu__item">
-										<a className="menu__item__link" href={FlowRouter.path("about")}>
+										<a className="menu__item-link" href={FlowRouter.path("about")}>
 											<T>menus.about</T>
 										</a>
 									</li>
 									<li className="menu__item">
-										<a className="menu__item__link" href={FlowRouter.path("contact")}>
+										<a className="menu__item-link" href={FlowRouter.path("contact")}>
 											<T>menus.contact</T>
 										</a>
 									</li>
 									<li className="menu__item">
-										<a className="menu__item__link" href={FlowRouter.path("authors")}>
+										<a className="menu__item-link" href={FlowRouter.path("authors")}>
 											<T>menus.authors</T>
 										</a>
 									</li>
-									{	
+									{
 										this.hasToDisplayDownloadBtn() &&
 											<li className="menu__item">
-												<a className="menu__item__link" href="#" onClick={installExtension}>
+												<a className="menu__item-link" href="#" onClick={installExtension}>
 													<T>menus.download</T>
 												</a>
 											</li>
@@ -68,7 +69,7 @@ class MenuFooter extends Component {
 									{
 										this.hasToDisplayProfileBtn() &&
 											<li className="menu__item">
-												<a 	className="menu__item__link" 
+												<a 	className="menu__item-link"
 													href={ FlowRouter.path("userProfile") }
 												>
 													profile
@@ -78,7 +79,7 @@ class MenuFooter extends Component {
 									{
 										this.hasToDisplayLogoutBtn() &&
 											<li className="menu__item">
-												<button className="menu__item__link" onClick={this.handleLogout.bind(this)}>
+												<button className="menu__item-link" onClick={this.handleLogout.bind(this)}>
 													<T>forms.logout</T>
 												</button>
 											</li>
@@ -90,13 +91,13 @@ class MenuFooter extends Component {
 							<div className="column__content">
 								<ul className="menu menu--footer">
 									<li className="menu__item">
-										<a className="menu__item__link" href={FlowRouter.path("about")}>github</a>
+										<a className="menu__item-link" href={FlowRouter.path("about")}>github</a>
 									</li>
 									<li className="menu__item">
-										<a className="menu__item__link" href={FlowRouter.path("about")}>facebook</a>
+										<a className="menu__item-link" href={FlowRouter.path("about")}>facebook</a>
 									</li>
 									<li className="menu__item">
-										<a className="menu__item__link" href={FlowRouter.path("contact")}>twitter</a>
+										<a className="menu__item-link" href={FlowRouter.path("contact")}>twitter</a>
 									</li>
 								</ul>
 							</div>
