6438c583724b33a125ee8764a11d3c81b80c22d8
oogre <vincent@ogre.be>
2019-05-04 17:35:57 +0200

    default language fr-FR

--- web.bcksp.es/imports/i18n/index.js
+++ web.bcksp.es/imports/i18n/index.js
@@ -2,10 +2,13 @@ import i18n from 'meteor/universe:i18n';
 
 import './fr.js';
 
-i18n.setLocale('fr-FR');
+
+
+//i18n.setLocale('en-US');
 i18n.setOptions({
 	//hostUrl : process.env.ROOT_URL,
-    purify: string => string
+    purify: string => string,
+    defaultLocale: 'fr-FR',
 });
 
 export default i18n.createComponent();
