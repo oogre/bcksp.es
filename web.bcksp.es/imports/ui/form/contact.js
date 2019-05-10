6a364a89c454b5dc2c31bd1d8ccbfb08be69755a
acr <hello@crucifixarnaud.com>
2019-05-10 09:58:46 +0200

    Souvenir: Contact us form design

--- web.bcksp.es/imports/ui/form/contact.js
+++ web.bcksp.es/imports/ui/form/contact.js
@@ -14,37 +14,37 @@ constructor(props){
 	render(){
 		const currentItem = i18n.createTranslator("souvenir.item.contact.form");
 		return (
-			<form onSubmit={this.props.onSubmit.bind(this)}>
-				{
-					!Meteor.userId() && 
-						<div className="fields-row">
-							<div className="fields-column">
-								<label>
-									<T>souvenir.item.contact.form.email.label</T>
+			<form className="shop-creation" onSubmit={this.props.onSubmit.bind(this)}>
+				<div className="shop-creation__order">
+					<div className="shop-creation__form">
+						{
+							!Meteor.userId() &&
+								<div className="fields-row">
+									<div className="field">
+										<label htmlFor="souvenirContactEmail" className="field__label">
+											<T>souvenir.item.contact.form.email.label</T>
+										</label>
+										<input id="souvenirContactEmail" className="input--text" type="email" name="contact.email"/>
+									</div>
+								</div>
+						}
+						<div className="">
+							<div className="field">
+								<label htmlFor="souvenirContactSubject" className="field__label">
+									<T>souvenir.item.contact.form.subject.label</T>
 								</label>
-								<input type="email" name="contact.email"/>
+								<input id="souvenirContactSubject" className="input--text" type="text" name="contact.subject"/>
+							</div>
+							<div className="field">
+								<label htmlFor="souvenirContactMessage" className="field__label">
+									<T>souvenir.item.contact.form.message.label</T>
+								</label>
+								<textarea id="souvenirContactMessage" className="textarea" type="text" name="contact.message"></textarea>
+							</div>
+							<div className="field">
+								<input className="button button--primary" type="submit" value={i18n.__("souvenir.item.contact.button")}/>
 							</div>
 						</div>
-				}
-				<div className="fields-row">
-					<div className="fields-column">
-						<label>
-							<T>souvenir.item.contact.form.subject.label</T>
-						</label>
-						<input type="text" name="contact.subject"/>
-					</div>
-				</div>
-				<div className="fields-row">
-					<div className="fields-column">
-						<label>
-							<T>souvenir.item.contact.form.message.label</T>
-						</label>
-						<textarea type="text" name="contact.message"></textarea>
-					</div>
-				</div>
-				<div className="fields-row">
-					<div className="fields-column">
-						<input type="submit" value={i18n.__("souvenir.item.contact.button")}/>
 					</div>
 				</div>
 			</form>
