3c12d9ab8ecc98bcbb35f2db7438c9561f0b7bcb
acr <hello@crucifixarnaud.com>
2019-05-24 10:56:50 +0200

    Add lang attribute to html

--- web.bcksp.es/client/main.js
+++ web.bcksp.es/client/main.js
@@ -1,20 +1,23 @@
 import { Meteor } from 'meteor/meteor';
 import '../imports/startup/account-config.js';
 import { isExtensionInstalled } from './../imports/utilities/ui.js';
-import './router.js'; 
+import './router.js';
 import { config } from './../imports/startup/config.js';
 
 Session.set("extensionInstalled", isExtensionInstalled());
 
-Meteor.startup(()=>{	
+Meteor.startup(()=>{
 	new MutationObserver(eventArray=>{
 		eventArray.map(event=>{
 			if(event.type == "attributes"){
 				Session.set("extensionInstalled", isExtensionInstalled());
 			}
 		})
-	}).observe( document.documentElement, { 
-		attributes: true 
+	}).observe( document.documentElement, {
+		attributes: true
 	});
 	i18n.setLocale(config.languages.get());
+
+	var htmlElement = document.querySelector("html");
+	htmlElement.setAttribute("lang", config.languages.get());
 });
