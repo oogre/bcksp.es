52b1495898ac599b346244bc9d3b24d02274c58f
oogre <vincent@ogre.be>
2020-02-04 22:04:52 +0100

    update meteor

--- web.bcksp.es/server/main.js
+++ web.bcksp.es/server/main.js
@@ -1,4 +1,5 @@
 import { Meteor } from 'meteor/meteor';
+import { setMinimumBrowserVersions } from "meteor/modern-browsers";
 // CLIENT & SERVER
 import '../imports/startup/account-config.js';
 import '../imports/api/archives/archives.js';
@@ -11,3 +12,29 @@ import './api/archives/archives.js';
 
 import '../imports/api/books/generator.js';
 import './../imports/i18n/index.js';
+
+setMinimumBrowserVersions({
+  chrome: 45,
+  firefox: 44,
+  edge: 17,
+  ie: Infinity,
+  mobileSafari: [10, 3],
+  opera: 32,
+  safari: [11, 1],
+  electron: [0, 36],
+}, "service workers");
+
+/*
+
+setMinimumBrowserVersions({
+  chrome: 49,
+  firefox: 45,
+  edge: 12,
+  ie: Infinity,
+  mobile_safari: [9, 2],
+  opera: 36,
+  safari: 9,
+  electron: 1,
+}, "minimum versions for ECMAScript 2015 classes");
+
+*/
