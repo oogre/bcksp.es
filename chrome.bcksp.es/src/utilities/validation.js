d9cf898ffc1e0c5289402085ba1fa3d1019bf6a7
oogre <vincent@ogre.be>
2021-03-02 22:57:49 +0100

    correcting Inefficient regular expression  : [firefox-chrome].bcksp.es/src/utilities/validation.js : isEmail

--- chrome.bcksp.es/src/utilities/validation.js
+++ chrome.bcksp.es/src/utilities/validation.js
@@ -2,7 +2,7 @@
   bcksp.es - validation.js
   @author Evrard Vincent (vincent@ogre.be)
   @Date:   2019-01-04 15:16:34
-  @Last Modified time: 2020-02-18 16:16:00
+  @Last Modified time: 2021-03-02 22:53:44
 \*----------------------------------------*/
 
 import Data from "./Data.js";
@@ -13,7 +13,8 @@ import { getContentEditableInParent, intersection, T } from "./tools.js";
 export { isString, isEmpty, isFunction, isUndefined, isNull, isArray, isBoolean, isObject } from 'underscore';
 
 export const regexp = {
-	email : /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+	email : /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+			
 }
 
 export const validation = {
